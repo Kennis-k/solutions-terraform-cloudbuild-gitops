866386d1aa65997b5823839bcf157e2dbfffe339
Kennis-k <kwskennis@gmail.com>
2022-03-31 09:03:23 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="argon-system-345807"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "argon-system-345807-tfstate"
     prefix = "env/prod"
   }
 }
